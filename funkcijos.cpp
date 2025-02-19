1d3b8e4d681e07d27ad9357c527f7631aa868c4e
Vancyy5 <balsytev05@.com>
2025-02-20 00:17:46 +0200

    Pridėti funkcijų failai

---
+++ funkcijos.cpp
@@ -0,0 +1,52 @@
+#include "funkcijos.h"
+
+double skaiciuotiVid(vector<int> nd) 
+{
+    if (nd.empty()) return 0.0;
+    else
+    {
+        double suma = 0.0;
+        for (int nd : nd) 
+        {
+            suma += nd;
+        }
+    return suma*1.00/ nd.size();
+    }
+    
+}
+//---
+double skaiciuotiMed(vector<int> nd) 
+{
+    if (nd.empty()) return 0.0;
+    else
+    {
+        sort(nd.begin(), nd.end());
+        int dydis = nd.size();
+    if (dydis % 2 == 0) 
+    {
+        return (nd[dydis / 2 - 1] + nd[dydis / 2]) / 2.0;
+    } 
+    else 
+    {
+        return nd[dydis / 2];
+    }
+}  
+}
+//---
+int readInteger() 
+{
+    int value;
+    while (true) {
+        cin >> value;
+        if (cin.fail() or value < 0) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Klaida: Prasome ivesti teigiama skaiciu." << endl;
+        }
+        else {
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+        }
+    }
+    return value;
+}
---
+++ funkcijos.h
@@ -0,0 +1,10 @@
+#ifndef FUNKCIJOS_H      
+#define FUNKCIJOS_H
+
+#include "lib.h"
+
+double skaiciuotiVid(vector <int> nd);
+double skaiciuotiMed(vector <int> nd);
+int readInteger();
+
+#endif // FUNKCIJOS_H
